79311ea8e3440e39294650b1e4fe50e46f8ddeec
PengFei Gao <v_pega@163.com>
2019-10-15 15:26:19 +0800

    update

--- src/Assembler.py
+++ src/Assembler.py
@@ -5,17 +5,20 @@ import os
 import pdb
 
 DEFAULTPATH='/Users/pengfeigao/git/assembler/test/add/Add.asm'
+DEFAULTPATH='/Users/pengfeigao/git/assembler/test/max/MaxL.asm'
+DEFAULTPATH='/Users/pengfeigao/git/assembler/test/pong/PongL.asm'
+DEFAULTPATH='/Users/pengfeigao/git/assembler/test/Rect/RectL.asm'
 
 class Assembler:
     def __init__(self, inputPath):
         with open(inputPath, 'r') as f:
             self.content = f.readlines()
-        fileName = inputPath.split('/')[-1][:-3]
+        fileName = inputPath.split('/')[-1][:-4]
         outputName = fileName+'.hack'
         self.outputPath = inputPath.replace(fileName+'.asm', outputName)
         self.hackCmds = []
 
-    def run(self, isWrite=False):
+    def run(self, isWrite=True):
         parser = Parser(self.content)
         hackCmds = self.hackCmds
         asmCmds = parser.asmCmds
@@ -27,22 +30,37 @@ class Assembler:
             if commandType in ['A','L']: 
                 symbol = parser.symbol(asmCmd)
                 print('symbol:', symbol)
+                hackCmds += ['0'+decimalToBinary15(symbol)+ '\n']
 
             elif commandType == 'C':
                 dest = parser.dest(asmCmd)
                 print('dest:', dest)
+                print('code dest',Code.dest(dest))
                 comp = parser.comp(asmCmd)
                 print('comp:', comp)
+                print('code comp',Code.comp(comp))
                 jump = parser.jump(asmCmd)
                 print('jump:', jump)
+                print('code jump',Code.dest(jump))
             
-                #hackCmds += ['111'+Code.comp(comp)+Code.dest(dest)+Code.jump(jump)]
+                hackCmds += ['111'+Code.comp(comp)+Code.dest(dest)+Code.jump(jump)+'\n']
+        print(hackCmds)
+        print(self.outputPath)
            
         if isWrite:
             with open(self.outputPath, 'w') as f:
-                f.write(''.join(asmCmds))
+                f.write(''.join(hackCmds))
          
-        
+def decimalToBinary15(decimal):
+    decimal = int(decimal)
+    binArr = []
+    while decimal:
+        reminder = decimal%2
+        binArr.append(str(reminder))
+        decimal = int(decimal/2)
+    offsetArr = ['0']*(15-len(binArr))
+    ret = ''.join(offsetArr+list(reversed(binArr)))
+    return ret
 
 if __name__ == '__main__':
     args = sys.argv
--- src/Code.py
+++ src/Code.py
@@ -2,12 +2,46 @@ class Code:
 
     @staticmethod
     def comp(cmd):
-        pass
+        if 'M' in cmd: a = '1'
+        else: a = '0'
+        cmd = cmd.replace('M','A')
+        ret = ''
+        if cmd == '0': ret = '101010'
+        elif cmd  == '1': ret = '111111' 
+        elif cmd  == '-1': ret = '111010' 
+        elif cmd  == 'D': ret = '001100' 
+        elif cmd  == '!D': ret = '001101' 
+        elif cmd  == '-D': ret = '001111' 
+        elif cmd  == 'D+1': ret = '011111' 
+        elif cmd  == 'D-1': ret = '001110' 
+        elif cmd  == 'A': ret = '110000' 
+        elif cmd  == '!A': ret = '110001' 
+        elif cmd  == '-A': ret = '110011' 
+        elif cmd  == 'A+1': ret = '110111' 
+        elif cmd  == 'A-1': ret = '110010' 
+        elif cmd  == 'D+A': ret = '000010' 
+        elif cmd  == 'D-A': ret = '010011' 
+        elif cmd  == 'A-D': ret = '000111' 
+        elif cmd  == 'D&A': ret = '000000' 
+        elif cmd  == 'D|A': ret = '010101' 
+        return a + ret
+        
 
     @staticmethod
     def dest(cmd):
-        pass
+        ret = [0,0,0]
+        if 'A' in cmd: ret[0] = 1
+        if 'D' in cmd: ret[1] = 1
+        if 'M' in cmd: ret[2] = 1
+        return ''.join([str(v) for v in ret])
 
     @staticmethod
     def jump(cmd):
-        pass
+        if not cmd: return '000'
+        elif cmd == 'JGT': return '001'
+        elif cmd == 'JEQ': return '010'
+        elif cmd == 'JGE': return '011'
+        elif cmd == 'JLT': return '100'
+        elif cmd == 'JNE': return '101'
+        elif cmd == 'JLE': return '110'
+        elif cmd == 'JMP': return '111'
